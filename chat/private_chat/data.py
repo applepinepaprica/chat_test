a0e87209041cb00917d109d8b1ff2073ea1e6549
Maria <maria.rozhkova@bravedevelopers.com>
2019-05-19 14:39:38 +0400

    Added tests

---
+++ chat/private_chat/data.py
@@ -0,0 +1,65 @@
+from graphene.test import Client
+from chat.schema import schema
+from .models import Message
+from django.contrib.auth.models import User
+import django.test
+
+
+def initialize():
+    user1 = User(
+        username="user1",
+        email="email@gmail.com"
+    )
+    user1.set_password("password")
+    user1.save()
+
+    user2 = User(
+        username="user2",
+        email="email@gmail.com"
+    )
+    user2.set_password("password")
+    user2.save()
+
+    message1 = Message(
+        sender=user1,
+        receiver=user2,
+        text="Text"
+    )
+
+    message1.save()
+
+    message2 = Message(
+        sender=user2,
+        receiver=user1,
+        text="Text"
+    )
+
+    message2.save()
+
+
+def get_token():
+    query = '''
+       mutation {
+          tokenAuth(username: "user1", password: "password") {
+            token
+          }
+        }'''
+    client = Client(schema)
+    result = client.execute(query)
+
+    return result.get('data').get('tokenAuth').get('token')
+
+
+def assert_test_without_auth(query, expected):
+    client = Client(schema)
+    result = client.execute(query)
+    assert result == expected
+
+
+def assert_test_with_auth(query, expected, token):
+    headers = {
+        'HTTP_AUTHORIZATION': f'JWT {token}'
+    }
+    client = django.test.Client()
+    response = client.get(f'/graphql/', query, content_type='application_json', **headers)
+    assert response.json() == expected
--- chat/private_chat/tests.py
+++ chat/private_chat/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .data import *
 
-# Create your tests here.
+
+class Tests(TestCase):
+
+    def setUp(self):
+        initialize()
+        self.token = get_token()
+
+    def test_create_user(self):
+        query = '''
+            mutation {
+              createUser(input: {username: "user", password: "password", email: "email@gmail.com"}) {
+                user {
+                  username
+                  email
+                }
+              }
+            }
+            '''
+        expected = {
+          "data": {
+            "createUser": {
+              "user": {
+                "username": "user",
+                "email": "email@gmail.com"
+              }
+            }
+          }
+        }
+        assert_test_without_auth(query, expected)
+
+    def test_get_me(self):
+        query = {"query": "{me {username email}}"}
+        expected = {
+          "data": {
+            "me": {
+              "username": "user1",
+              "email": "email@gmail.com"
+            }
+          }
+        }
+        assert_test_with_auth(query, expected, self.token)
