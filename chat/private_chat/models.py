bde4978a210958879241ec96081558f148508b4f
Maria <maria.rozhkova@bravedevelopers.com>
2019-05-21 10:21:02 +0400

    Added Dialogue

--- chat/private_chat/models.py
+++ chat/private_chat/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 class Message(models.Model):
     sender = models.ForeignKey(User, null=False, on_delete=models.PROTECT, related_name='+')
-    receiver = models.ForeignKey(User, null=False, on_delete=models.PROTECT, related_name='+')
     text = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
+
+
+class Dialogue(models.Model):
+    user1 = models.ForeignKey(User, null=False, on_delete=models.PROTECT, related_name='+')
+    user2 = models.ForeignKey(User, null=False, on_delete=models.PROTECT, related_name='+')
